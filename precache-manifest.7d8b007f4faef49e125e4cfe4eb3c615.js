2f5a64b3fb4a28546b878537f4e984ac10c41830
DeepakMiddha <middha24@gmail.com>
2019-04-30 02:17:50 +0530

    Updates

---
+++ precache-manifest.7d8b007f4faef49e125e4cfe4eb3c615.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e2313f07c1130529320d409df8d8bea",
+    "url": "/home/static/media/home_pic.6e2313f0.png"
+  },
+  {
+    "revision": "4e1ec8947a6857600002",
+    "url": "/home/static/js/runtime~main.0990ee84.js"
+  },
+  {
+    "revision": "207f2043942a3792434d",
+    "url": "/home/static/js/main.69fc3a2a.chunk.js"
+  },
+  {
+    "revision": "991bcd1fe02eb18ef00b",
+    "url": "/home/static/js/2.c174cbbf.chunk.js"
+  },
+  {
+    "revision": "207f2043942a3792434d",
+    "url": "/home/static/css/main.a06b62e4.chunk.css"
+  },
+  {
+    "revision": "4e4cf1831f77a176474ca01db0e5adc9",
+    "url": "/home/index.html"
+  }
+];
